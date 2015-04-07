3db23cb66148d1f059558cb54d9a8ab33357e4b1
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-04-07 12:53:52 +0800

    Ignore vagrant files. Bootstrap VM.

--- build-tools/virtual-machine/scripts/bootstrap.sh
+++ build-tools/virtual-machine/scripts/bootstrap.sh
@@ -5,4 +5,4 @@ apt-get update
 
 # Build essential is needed because we might want local machine unit testing.
 # Unit testing with Arduino hardware is difficult.
-apt-get install -y arduino-core arduino-mk build-essential git libcppunit-dev
+apt-get install -y build-essential git mercurial libcppunit-dev
