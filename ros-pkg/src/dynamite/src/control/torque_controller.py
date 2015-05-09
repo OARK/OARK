dc1a269b72163293050d926519e0f19cba9e1736
Tim Peskett <tdpeskett@gmail.com>
2015-05-09 15:10:02 +0000

    Fixed controllers

--- ros-pkg/src/dynamite/src/control/torque_controller.py
+++ ros-pkg/src/dynamite/src/control/torque_controller.py
@@ -4,28 +4,62 @@
 # A torque controller controls a joint and applies
 # a constant torque to it until otherwise desired.
 #
+# For now we assume that all of the controller parameters
+# are already on the parameter server. This will be changed
+# in future to allow easy dynamic controller creation.
+
 # The prototypical nature of this class means that it
 # currently has many thorns. It should be used with
 # caution lest the user be pricked.
 
 
+
 import controller
 import threading
+import manager_proxy
 
 
 class TorqueController(controller.Controller):
-    def __init__(self, proxy, controller_name):
+    def __init__(self, proxy, controller_name, port_ns):
         #Initialise fields of parent class
-        controller.Controller.__init__(self)
+        #Creates object fields: self.proxy, self.controller_name, and
+        # self.port_ns
+        controller.Controller.__init__(self, proxy, controller_name, port_ns)
 
         #Create controller
         proxy.start_torque_cont(controller_name, port_ns)
 
     def destroy(self):
-        proxy.stop_torque_cont(self.controller_name, self.port_ns)
+        self.proxy.stop_torque_cont(self.controller_name, self.port_ns)
 
     def command(self, value):
         self.proxy.command(self.controller_name, value)
 
-    def set_torque(self, position):
-        self.proxy.command(self.controller_name, torque)
+    #TODO - Specify torque in sensible unit
+    def set_torque(self, torque):
+        self.command(torque)
+
+    def stop(self):
+        self.set_torque(0)
+
+
+
+#Used only for testing
+import rospy
+import time
+
+#Cute little test harness.
+if __name__ == '__main__':
+    rospy.init_node('torque_test_node')
+    man_proxy = manager_proxy.ManagerProxy('dxl_manager')
+    pos = TorqueController(man_proxy, 'wheel_fl', 'pi_out_port')
+    time.sleep(3)
+    pos.command(2.0)
+    time.sleep(3)
+    pos.command(0.0)
+    time.sleep(3)
+    pos.command(-2.0)
+    time.sleep(3)
+    pos.command(0.0)
+    rospy.spin()
+
