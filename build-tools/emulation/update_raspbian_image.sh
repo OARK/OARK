afa4fbb6e59454750df4b6893def97397504a4a3
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-06-04 13:59:26 +0800

    Messages on stages of image updating for emulator.

--- build-tools/emulation/update_raspbian_image.sh
+++ build-tools/emulation/update_raspbian_image.sh
@@ -14,6 +14,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Colour codes
+COLOUR_SUCCESS='\033[0;32m'
+COLOUR_PROGRESS='\033[0;33m'
+NC='\033[0m' # No Color
+
 updateLdPreload() {
     sed -i -e "/libcofi_rpi/s/^/#/g" $1/etc/ld.so.preload
 }
@@ -26,6 +31,8 @@ KERNEL=="sda2", SYMLINK+="root"
 EOF
 }
 
+# Add SSH keys so we don't need a password to login from the host.
+# This is to make scripting the guest VM easier.
 addSSHKeys() {
     mkdir -p $1/home/pi/.ssh
     cp ~/.ssh/id_rsa.pub $1/home/pi/.ssh/authorized_keys
@@ -33,8 +40,18 @@ addSSHKeys() {
     chmod 700 $1/home/pi/.ssh
     chmod 644 $1/home/pi/.ssh/authorized_keys
 
+    # User 1000 is the "pi" user account.
     chown -R 1000:1000 $1/home/pi/.ssh
 }
 
+echo -e "${COLOUR_PROGRESS}Updating ld.so.preload...${NC}"
 updateLdPreload $1
+echo -e "${COLOUR_SUCCESS}Update ld.so.preload, success.${NC}"
+
+echo -e "${COLOUR_PROGRESS}Updating udev disks...${NC}"
 updateUdevDisks $1
+echo -e "${COLOUR_SUCCESS}Update udev disks, success.${NC}"
+
+echo -e "${COLOUR_PROGRESS}Adding SSH keys to Pi image...${NC}"
+addSSHKeys $1
+echo -e "${COLOUR_SUCCESS}Update SSH keys to Pi, success.${NC}"
