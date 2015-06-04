4beeb57db9f9d6051d5311d021586e7d3c1284fc
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-06-04 11:05:04 +0800

    Need unzip in build VM, also Vagrant is stupid with file copying.

--- build-tools/virtual-machine/scripts/bootstrap.sh
+++ build-tools/virtual-machine/scripts/bootstrap.sh
@@ -5,7 +5,7 @@ apt-get update
 
 # Build essential is needed because we might want local machine unit testing.
 # Unit testing with Arduino hardware is difficult.
-apt-get install -y bc build-essential git mercurial
+apt-get install -y bc build-essential git mercurial unzip
 
 # For Pi emulation
 apt-get install -y qemu-system-arm
